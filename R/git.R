6a694f672e78c7a91ea97907c238e5446234a989
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-08-23 09:18:09 +0100

    added uses_git check function

--- R/git.R
+++ R/git.R
@@ -2,3 +2,13 @@ init_git <- function() {
   usethis::use_git()
   invisible(TRUE)
 }
+
+uses_git <- function(base_path = proj_get()) {
+  tryCatch({
+    gert::git_info(repo = base_path)
+    TRUE
+  },
+  error = function(c) {
+    FALSE
+  })
+}
